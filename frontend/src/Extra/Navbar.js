4d1f01e79338b10000942b2972bec1056e0a6e6e
JuanAriza17 <60227446+JuanAriza17@users.noreply.github.com>
2024-11-19 04:32:00 -0500

    actualizaciones navbar

--- frontend/src/Extra/Navbar.js
+++ frontend/src/Extra/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-
+import logo from "../images/logo.png"
 
 function Navbar() {
   const navigate = useNavigate();
@@ -19,20 +19,20 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <Link className="navbar-brand" to={localStorage.getItem("token")?"/principal":"/"}>CyberLearn</Link>
+        <Link className="navbar-brand" to={localStorage.getItem("token")?"/principal":"/"}><img src={logo} alt="logo" style={{ height: '40px', width: 'auto' }}/> CyberLearn</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link" to="/courses">Cursos</Link>
-            </li>
             {token && (
               <li className="nav-item">
                 <Link className="nav-link" to="/dashboard">Dashboard</Link>
               </li>
             )}
+            <li className="nav-item">
+              <Link className="nav-link" to="/courses">Cursos</Link>
+            </li>
             <li className="nav-item">
               <Link className="nav-link" to="/help">Ayuda</Link>
             </li>
